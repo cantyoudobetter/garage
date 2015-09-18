51d88174146d16f956d51921da73cf2f9b82ae4c
cantyoudobetter <michael.d.bordelon@gmail.com>
2015-09-18 16:20:26 +0000

    pi support

--- o.php
+++ o.php
@@ -1,5 +1,5 @@
 <?php
-
+        //PI Version
 	$comPort = "/dev/ttyACM0"; /*change to correct com port */
   	$fp =fopen($comPort, "w");
   	fwrite($fp, "O"); /* this is the number that it will write */
